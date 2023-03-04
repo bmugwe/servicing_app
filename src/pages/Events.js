9469733111e7fc147177ce348840d757d076aba8
Mugwe B <102720427+bmugwe@users.noreply.github.com>
2023-03-04 11:22:13 +0300

    first commit

---
+++ src/pages/Events.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Events = () => {
+return (
+	<div className="events">
+	<h1>Events</h1>
+	</div>
+);
+};
+
+export const EventsOne = () => {
+return (
+	<div className="events">
+	<h1>Upcoming Events</h1>
+	</div>
+);
+};
+
+export const EventsTwo = () => {
+return (
+	<div className="events">
+	<h1>Expired Events</h1>
+	</div>
+);
+};
